efa9b0627d0de3ded43db8643963ed18314e6486
placid-computer-9089 <zubairmaqsood866@gmail.com>
2019-02-18 02:33:30 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -2,12 +2,11 @@
 
 def loop_iterator(number_of_times)
 counter = 0 
-user_input = gets.chomp 
   loop do
     counter += 1 
     puts "Welcome to Flatiron School's Web Development Course!"
     
-    if counter == user_input 
+    if counter == number_of_times
       break
     end 
 end
